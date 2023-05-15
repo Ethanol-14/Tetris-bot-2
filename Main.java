1509b02c4ca09459f728f2c0b80299f3337e5366
Ethanol-14 <69091472+Ethanol-14@users.noreply.github.com>
2023-05-15 15:07:18 -0700

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,9 +1,42 @@
-package tetrismode;
 
 public class Main {
-	
-	public static void main(String[] args) {
-		Decision.FindBestPlacement(null, Board.GetBoard());
+	public static void main (String[] args) {
+		System.out.println("test");
+		
+		TestT(5);
 	}
 	
+	private static void TestT(int y) {
+		byte[] queue = new byte[1];
+		queue[0] = 6;
+		byte[] finals = Decision.FindBestPlacement(queue, Board.GetBoard());
+		
+		System.out.println(finals[0]);
+		System.out.println(finals[1]);
+		
+		if (finals[1] == 0) { //T pointing down
+			Board.EditBoard(3+finals[0], y, 1);
+			Board.EditBoard(4+finals[0], y, 1);
+			Board.EditBoard(5+finals[0], y, 1);
+			Board.EditBoard(4+finals[0], y+1, 1);
+		}
+		else if (finals[1] == 1) { //T pointing left
+			Board.EditBoard(4+finals[0], y, 1);
+			Board.EditBoard(4+finals[0], y+1, 1);
+			Board.EditBoard(4+finals[0], y+2, 1);
+			Board.EditBoard(5+finals[0], y+1, 1);
+		}
+		else if (finals[1] == 2) { //T
+			Board.EditBoard(4+finals[0], y, 1);
+			Board.EditBoard(3+finals[0], y+1, 1);
+			Board.EditBoard(4+finals[0], y+1, 1);
+			Board.EditBoard(5+finals[0], y+1, 1);
+		}
+		else { //T pointing right
+			Board.EditBoard(3+finals[0], y+1, 1);
+			Board.EditBoard(4+finals[0], y, 1);
+			Board.EditBoard(4+finals[0], y+1, 1);
+			Board.EditBoard(4+finals[0], y+2, 1);
+		}
+	}
 }
